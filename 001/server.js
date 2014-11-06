cfee7dac42b932a83bef9261cf0903a1eef24235
Hiroki Sato <m@skahack.com>
2014-11-06 22:50:10 +0900

    001: tutorial 15

--- 001/server.js
+++ 001/server.js
@@ -1,10 +1,20 @@
 var debug = require('debug')('001');
 var fs = require('fs');
 var path = require('path');
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
+var comments = JSON.parse(fs.readFileSync('_comments.json'));
+
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/comments.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(comments));
+});
 
 app.listen(3000);
 
