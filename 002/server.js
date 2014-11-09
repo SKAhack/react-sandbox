560eee195c69db8d0e85a13fe478c757119e0b87
Hiroki Sato <m@skahack.com>
2014-11-10 02:10:40 +0900

    start 002. reactjs + flux

---
+++ 002/server.js
@@ -0,0 +1,10 @@
+var debug = require('debug')('002');
+var path = require('path');
+var express = require('express');
+var app = express();
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.listen(3000);
+
+debug('Server started: http://localhost:3000/');
